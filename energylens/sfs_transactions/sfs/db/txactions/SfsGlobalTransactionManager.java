091fbefc777253155b6b9e4ebe511b38616f0488
Ubuntu <ubuntu@ip-10-171-47-100.us-west-1.compute.internal>
2012-07-24 07:05:50 +0000

    created a comparator to sort the incoming log dump;

--- energylens/sfs_transactions/sfs/db/txactions/SfsGlobalTransactionManager.java
+++ energylens/sfs_transactions/sfs/db/txactions/SfsGlobalTransactionManager.java
@@ -2,6 +2,7 @@ package sfs.db.txactions;
 
 import sfs.db.*;
 import java.net.*;
+import java.util.*;
 
 import org.simpleframework.transport.connect.Connection;
 import org.simpleframework.transport.connect.SocketConnection;
@@ -46,20 +47,27 @@ public class SfsGlobalTransactionManager implements Container{
     }
 
     public void handle(Request request, Response response){
+        try {
         String method = request.getMethod();
         if(method.equalsIgnoreCase("PUT") || method.equalsIgnoreCase("POST")){
-            JSONObject contentObj = parser.parse(request.getContent());
-            String type = contentObj.optString("type");
-            JSONArray log = contentObj.optJSONArray("ops");
+            JSONObject contentObj = (JSONObject)parser.parse(request.getContent());
+            String type = (contentObj.get("type")!=null)?((String)contentObj.get("type")):"";
+            JSONArray log = (contentObj.get("ops")!=null)?(JSONArray)contentObj.get("ops"):null;
             if(type.equals("log")){
-                /*for(int i=0; i<log.size(); i++){
-                }*/
+                
                 //sort these by timestamp
-                //try to apply it in timestamp order
-                //  -- check the log, if it's the latest one, apply it, if not, resolve the conflict
+                JSONObject[] log_array = (JSONObject[])log.toArray();
+                Arrays.sort(log_array, new LogEntryComparator<org.json.simple.JSONObject>());
+                for(int i=0; i<log.size(); i++)
+                    applyAttempt(log_array[i]);    
+                
+                
             } else {
-                forwardOpToSfs(method, request.getPath(), contentObj);
+                forwardOpToSfs(method, request.getPath().getPath(), contentObj);
             }
+        }
+        } catch(Exception e){
+            logger.log(Level.WARNING, "", e);
         }
     }
 
@@ -67,6 +75,13 @@ public class SfsGlobalTransactionManager implements Container{
      * handle create/delete [default|symlink] resources, update/overwrite properties
      */
     public void forwardOpToSfs(String method, String path, JSONObject data ){
+    }
+
+    public void applyAttempt(JSONObject sfsop){
+        long ts = ((Long)sfsop.get("ts")).longValue();
+        JSONArray getAllOpsAfter(ts);
+        //try to apply it in timestamp order
+        //  -- check the log, if it's the latest one, apply it, if not, resolve the conflict
     }
 
     public synchronized void log(JSONObject sfsop){
@@ -83,5 +98,40 @@ public class SfsGlobalTransactionManager implements Container{
      */
     public synchronized void resolveConflict(JSONObject sfsop){
     }
-    
+   
+    private class LogEntryComparator<E> implements Comparator<E>{
+        public int compare (E e1, E e2){
+            if(!(e1 instanceof JSONObject) && !(e2 instanceof JSONObject))
+                throw new ClassCastException();
+            JSONObject entry1 = (JSONObject) e1;
+            JSONObject entry2 = (JSONObject) e2;
+            if(entry1!=null && entry2 !=null){
+                long ts1=0L;
+                long ts2=0L;
+                if(entry1.get("ts")!=null)
+                    ts1 = ((Long)entry1.get("ts")).longValue();
+                if(entry2.get("ts")!=null)
+                    ts2 = ((Long)entry2.get("ts")).longValue();
+
+                if(ts1<ts2)
+                    return -1;
+                else if(ts1==ts2)
+                    return 0;
+                else if(ts1>ts2)
+                    return 1;
+            } else {
+                throw new NullPointerException();
+            }
+            
+            return -2;
+        }
+
+        public boolean equals(E e1, E e2){
+            try {
+                if(this.compare(e1, e2)==0)
+                    return true;
+            } catch(Exception e){}
+            return false;
+        }
+    }
 }
