01b4f00c8a62167202191d0b0cbab2137a4645a1
Ubuntu <ubuntu@ip-10-171-47-100.us-west-1.compute.internal>
2012-07-26 18:42:12 +0000

    updated tracewriter to write traces for sample04 and sample04, started sample05 method

--- energylens/sfs_transactions/test/TraceWriter.java
+++ energylens/sfs_transactions/test/TraceWriter.java
@@ -9,10 +9,14 @@ public class TraceWriter{
     public static final long sfstime = 1343286861L;
     private static final String path = "/xactions_tests";
     public static void main(String[] args){
+        System.out.println("#####   makeSample01 #####");
         makeSample01();
-        System.out.println("\n\n\n");
+        System.out.println("\n\n\n#####   makeSample02 #####");
         makeSample02();
-        System.out.println("\n\n\n");
+        System.out.println("\n\n\n#####   unmakeSample02 #####");
+        unmakeSample02();
+        System.out.println("\n\n\n#####   makeSample03 #####");
+        makeSample03();
     }
 
     public static void makeSample01(){
@@ -27,17 +31,82 @@ public class TraceWriter{
         JSONObject log = new JSONObject();
         JSONArray ops = new JSONArray();
 
-        JSONObject data = new JSONObject();
-        data.put("operation", "create_resource");
-        data.put("resourceType", "default");
-     
         Random rand = new Random();
         long opts = sfstime;// + rand.nextInt( );
         log.put("type", "log"); 
         for(int i=0; i<10; i++){
             JSONObject entry = new JSONObject();
+            JSONObject data = new JSONObject();
+            data.put("operation", "create_resource");
+            data.put("resourceType", "default");
             entry.put("ts", opts);
             opts = opts + 2*(i+1);
+            if(i==0){
+                data.put("resourceName", "spaces");
+                entry.put("path", path);
+            } else if(i==2){
+                data.put("resourceName", "dev");
+                entry.put("path", path);
+            } else {
+                data.put("resourceName", "PC" + (new Integer(i)).toString());
+                entry.put("path", path + "/dev");
+            }
+            entry.put("op", "put");
+            entry.put("data", data);
+            ops.add(entry);
+        }
+        log.put("ops", ops);
+        System.out.println(log.toString());
+    }
+
+    public static void unmakeSample02(){
+        JSONObject log = new JSONObject();
+        JSONArray ops = new JSONArray();
+
+        Random rand = new Random();
+        long opts = sfstime+2000*3;// + rand.nextInt( );
+        log.put("type", "log"); 
+        for(int i=9; i>=0; i--){
+            JSONObject entry = new JSONObject();
+            entry.put("ts", opts);
+            opts = opts + 2*(i+1);
+            if(i==0){
+                entry.put("path", path + "/spaces");
+            } else if(i==1){
+                entry.put("path", path + "/dev");
+            } else {
+                entry.put("path", path + "/dev/PC" + (new Integer(i)).toString());
+            }
+            entry.put("op", "delete");
+            ops.add(entry);
+        }
+        log.put("ops", ops);
+        System.out.println(log.toString());
+    }
+
+    public static void makeSample03(){
+        JSONObject log = new JSONObject();
+        JSONArray ops = new JSONArray();
+        JSONObject log2 = new JSONObject();
+        JSONArray ops2 = new JSONArray();
+
+        Random rand = new Random();
+        long opts = sfstime;// + rand.nextInt( );
+        log.put("type", "log"); 
+        for(int i=0; i<20; i+=2){
+            JSONObject entry = new JSONObject();
+            JSONObject data = new JSONObject();
+            data.put("operation", "create_resource");
+            data.put("resourceType", "default");
+            entry.put("ts", opts);
+
+            JSONObject entry2 = new JSONObject();
+            JSONObject data2 = new JSONObject();
+            data2.put("operation", "create_resource");
+            data2.put("resourceType", "default");
+            entry2.put("ts", opts+1);
+            opts = opts + 2*(i+1);
+
             if(i==0){
                 data.put("resourceName", "spaces");
                 entry.put("path", path);
@@ -47,9 +116,75 @@ public class TraceWriter{
             } else {
                 data.put("resourceName", "r" + (new Integer(i)).toString());
                 entry.put("path", path + "/dev");
+                data2.put("resourceName", "r" + (new Integer(i+1)).toString());
+                entry2.put("path", path + "/dev");
             }
             entry.put("op", "put");
             entry.put("data", data);
+            entry2.put("op", "put");
+            entry2.put("data", data2);
+            ops.add(entry);
+            ops2.add(entry2);
+        }
+        log.put("ops", ops);
+        log2.put("ops", ops2);
+        System.out.println(log.toString());
+        System.out.println("\n\n\n");
+        System.out.println(log2.toString());
+    }
+
+
+    public static void makeSample05(){
+        JSONObject log = new JSONObject();
+        JSONArray ops = new JSONArray();
+
+        Random rand = new Random();
+        long opts = sfstime;// + rand.nextInt( );
+        log.put("type", "log"); 
+        for(int i=0; i<10; i++){
+            JSONObject entry = new JSONObject();
+            JSONObject data = new JSONObject();
+            data.put("operation", "create_resource");
+            data.put("resourceType", "default");
+            entry.put("ts", opts);
+            opts = opts + 2*(i+1);
+            if(i==0){
+                data.put("resourceName", "spaces");
+                entry.put("path", path);
+            } else if(i==2){
+                data.put("resourceName", "dev");
+                entry.put("path", path);
+            } else {
+                data.put("resourceName", "PC" + (new Integer(i)).toString());
+                entry.put("path", path + "/dev");
+            }
+            entry.put("op", "put");
+            entry.put("data", data);
+            ops.add(entry);
+        }
+        log.put("ops", ops);
+        System.out.println(log.toString());
+    }
+
+    public static void unmakeSample02(){
+        JSONObject log = new JSONObject();
+        JSONArray ops = new JSONArray();
+
+        Random rand = new Random();
+        long opts = sfstime+2000*3;// + rand.nextInt( );
+        log.put("type", "log"); 
+        for(int i=9; i>=0; i--){
+            JSONObject entry = new JSONObject();
+            entry.put("ts", opts);
+            opts = opts + 2*(i+1);
+            if(i==0){
+                entry.put("path", path + "/spaces");
+            } else if(i==1){
+                entry.put("path", path + "/dev");
+            } else {
+                entry.put("path", path + "/dev/PC" + (new Integer(i)).toString());
+            }
+            entry.put("op", "delete");
             ops.add(entry);
         }
         log.put("ops", ops);
