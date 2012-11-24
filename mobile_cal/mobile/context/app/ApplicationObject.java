d90c0cc2e1f493c9d64be0ea5231ec0126d3b030
Jorge Ortiz <jortiz@cs.berkeley.edu>
2012-11-24 14:45:40 -0800

    moved the application object and it's name object to the app folder

--- mobile_cal/mobile/context/app/ApplicationObject.java
+++ mobile_cal/mobile/context/app/ApplicationObject.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * Represents the basic unit of operation for the application.  ApplicationObjects represent 
  * an object in the application that can be mutated by applied an Operation object on it.
@@ -6,18 +8,36 @@ public abstract class ApplicationObject{
 
     private ObjectName name = null;
 
-    public Applicationobject(ObjectName n){
+    public ApplicationObject(ObjectName n){
         name = n;
     }
    
     /**
      * Returns the name of the object as represented by the ObjectName.
      */
-    public ObjectName getName(){}
+    public ObjectName getName(){
+        return name;
+    }
 
     /**
      * Returns of copy of this object.  Used to keep a history of the object before and after the application of an operation.
      */
-    public ApplicationObject copyObject();
+    public ApplicationObject clone(){
+        return (ApplicationObject)this.clone();
+    }
+
+    /**
+     * Returns a byte array representation of this application object.
+     */
+    public abstract byte[] getBytes();
+
+    public boolean equals(ApplicationObject appObj){
+        if(appObj==null)
+            return false;
+        if(Arrays.equals(appObj.getName().getByteName(), name.getByteName()) && 
+                Arrays.equals(appObj.getBytes(), getBytes()))
+            return true;
+        return false;
+    }
 
 }
