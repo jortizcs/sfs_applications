d9b9dcdc42e62dc9dc665e62ad1d22584277500e
Jorge Ortiz <jortiz@cs.berkeley.edu>
2012-11-28 15:31:58 -0800

    updates the interface

--- mobile_cal/mobile/context/app/ApplicationServer.java
+++ mobile_cal/mobile/context/app/ApplicationServer.java
@@ -3,11 +3,11 @@ package mobile.context.app;
 import mobile.context.client.*;
 
 public interface ApplicationServer{
-    public ApplicationObject doRead();
+    public ApplicationObject doRead(ObjectName objectName);
 
     public ApplicationObject doWrite(Operation op);
 
     public ApplicationObject[] doWriteExpession(Expression exp);
 
-    public byte[] doQuery(Operation op);
+    public byte[] doQuery(String queryStrin);
 }
