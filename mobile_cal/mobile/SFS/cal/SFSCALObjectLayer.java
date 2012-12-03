44ba4d39c5717411d175ff10e1e00b833ad3c34f
Jorge Ortiz <jortiz@cs.berkeley.edu>
2012-12-02 22:29:05 -0800

    updated the same to match the layer name

--- mobile_cal/mobile/SFS/cal/SFSCALObjectLayer.java
+++ mobile_cal/mobile/SFS/cal/SFSCALObjectLayer.java
@@ -3,7 +3,7 @@ package mobile.SFS.cal;
 import mobile.context.app.*;
 import mobile.context.client.*;
 
-public class SFSContextObjectLayer extend ContextObjectLayer{
+public class SFSCALObjectLayer extend CALObjectLayer{
     public ApplicationObject read(ObjectName objectName){
         return null;
     }
